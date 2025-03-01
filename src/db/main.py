7d7d07045e6ba484a5e93eab2bb23f4cee47b791
KaranKumar-55 <karankumarladkani@gmail.com>
2025-03-02 02:56:02 +0500

    Adding dependency Injection

--- src/db/main.py
+++ src/db/main.py
@@ -1,16 +1,18 @@
 from sqlmodel import create_engine, text, SQLModel
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
-from sqlalchemy.future import select
+from sqlalchemy.orm import sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-engine = AsyncEngine(
+
+async_engine = AsyncEngine(
     create_engine(
     url = Config.DATABASE_URL,
     echo = True)
 )
 
 async def init_db():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         from src.books.models import Book
         await conn.run_sync(SQLModel.metadata.create_all)
 
@@ -19,4 +21,11 @@ async def init_db():
         # result = await conn.execute(statement)
         # print(result.all())
 
-    
+async def get_session()-> AsyncSession:
+    Session = sessionmaker(
+        bind = async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+    async with Session() as session:
+        yield session
